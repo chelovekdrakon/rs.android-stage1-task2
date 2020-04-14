931fbc4067e95d1ce11512067d2af5053b611611
Morau Fiodar <MorauFiodar@yahoo.com>
2020-04-14 21:41:57 +0300

    feat: Fibonacci.productFibonacciSequenceFor method (Exercise 6)

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,18 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        var prev = 0
+        var curr = 1
+        var temp: Int
+        var multiplied = prev * curr
+
+        while (multiplied < n) {
+            temp = curr;
+            curr += prev;
+            prev = temp;
+            multiplied = prev * curr
+        }
+
+        return intArrayOf(prev, curr, if (multiplied === n) 1 else 0)
     }
 }
