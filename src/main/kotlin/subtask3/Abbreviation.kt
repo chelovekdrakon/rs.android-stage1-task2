23dad25401b223545d61f7683dea6ebc864102bb
Morau Fiodar <MorauFiodar@yahoo.com>
2020-04-14 21:30:35 +0300

    feat: Abbreviation abbreviationFromA method (Exercise 3)

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,20 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val temp = StringBuilder(a.toUpperCase())
+        var i = 0
+        while (b.length != i && b.length <= temp.length){
+            if (temp[i] != b[i]){
+                temp.deleteCharAt(i)
+                i--
+            }
+            i++
+        }
+
+        return if (i == b.length){
+            "YES"
+        } else {
+            "NO"
+        }
     }
 }
