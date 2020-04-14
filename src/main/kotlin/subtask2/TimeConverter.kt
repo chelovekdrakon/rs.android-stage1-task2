ea8d1d7a7d776c4a0396e01321c645ddb7e6fce8
Morau Fiodar <MorauFiodar@yahoo.com>
2020-04-14 21:26:51 +0300

    feat: TimeConverter toTextFormat method (Exercise 2)

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -1,9 +1,49 @@
 package subtask2
 
 class TimeConverter {
+    val mapNumberToWord = arrayOf(
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty",
+        "twenty one",
+        "twenty two",
+        "twenty three",
+        "twenty four",
+        "twenty five",
+        "twenty six",
+        "twenty seven",
+        "twenty eight",
+        "twenty nine",
+        "thirty")
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        return when (minute.toInt()) {
+            0 -> "${mapNumberToWord[hour.toInt()]} o' clock"
+            1 -> "one minute past ${mapNumberToWord[hour.toInt()]}"
+            15 -> "quarter past ${mapNumberToWord[hour.toInt()]}"
+            30 -> "half past ${mapNumberToWord[hour.toInt()]}"
+            45 -> "quarter to ${mapNumberToWord[hour.toInt() + 1]}"
+            in 1..30 -> "${mapNumberToWord[minute.toInt()]} minutes past ${mapNumberToWord[hour.toInt()]}"
+            in 31..60 -> "${mapNumberToWord[60 - minute.toInt()]} minutes to ${mapNumberToWord[hour.toInt() + 1]}"
+            else -> ""
+        }
     }
 }
