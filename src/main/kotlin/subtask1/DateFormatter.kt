6a2b084d2d37a8b1f55a20f73f8b83f2aabc1b4b
Morau Fiodar <MorauFiodar@yahoo.com>
2020-04-14 21:16:32 +0300

    feat: DateFormatter toTextDay method (Exercise 1)

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,31 @@
 package subtask1
 
-class DateFormatter {
+import java.lang.StringBuilder
+import java.time.LocalDate
+import java.time.format.DateTimeParseException
 
-    // TODO: Complete the following function
+class DateFormatter {
+    val monthArray = arrayOf("января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря")
+
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        return try {
+            val m  = if (month.length == 1) "0$month" else month
+            val date = LocalDate.parse("$year-$m-$day")
+
+            val month = monthArray[month.toInt() - 1];
+
+            when (date.dayOfWeek.toString()){
+                "SUNDAY"->"$day $month, воскресенье"
+                "MONDAY"->"$day $month, понедельник"
+                "TUESDAY"->"$day $month, вторник"
+                "WEDNESDAY"->"$day $month, среда"
+                "THURSDAY"->"$day $month, четверг"
+                "FRIDAY"->"$day $month, пятница"
+                "SATURDAY"->"$day $month, суббота"
+                else -> "Такого дня не существует"
+            }
+        } catch(e: DateTimeParseException) {
+            "Такого дня не существует"
+        }
     }
 }
